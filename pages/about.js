a9f55ac2cf0638e5907d6ac239e9b203b16be960
Maria Bohorquez <maria.bohorquez@ceiba.com.co>
2022-06-13 17:33:02 -0500

    feature: add route static

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        <h1>Esta es la pagina de about</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>
+        <h1>Hola mundo platzi</h1>
+    </div>
+  );
+};
+
+export default Home;
