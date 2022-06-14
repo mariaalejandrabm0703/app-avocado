2ee9cb7d365cee62c500df2cbf4d61d0ad9cbff0
Maria Bohorquez <maria.bohorquez@ceiba.com.co>
2022-06-14 17:57:13 -0500

    feature: add view detail product

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 
 export const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await window.fetch("api/avocado");
+        const { data } = await response.json();
+        setProducts(data);
+      } catch (error) {
+        setProducts([]);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <Navbar />
+      {products?.map((product) => {
+        return <h1 key={product.id}> {product.name} </h1>;
+      })}
       <h1>Hola mundo platzi</h1>
     </div>
   );
--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,15 +1,56 @@
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import React from "react";
-import Navbar from "../../components/Navbar/Navbar"
+import Navbar from "../../components/Navbar/Navbar";
 
-const ProductItem = () => {
-  const router = useRouter();
+const ProductPage = () => {
+  const [product, setProduct] = useState({});
+
+  const {
+    query: { id },
+  } = useRouter();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await window.fetch(`/api/avocado/${id}`);
+        const { data } = await response.json();
+        setProduct(data);
+      } catch (error) {
+        setProduct({});
+      }
+    };
+
+    fetchProducts();
+  }, [id]);
+
   return (
-    <div>
+    <section>
       <Navbar />
-      <h1>Pagina para el producto {router.query.id}</h1>
-    </div>
+      <section>
+        <h1>Página producto</h1>
+        {product ? (
+          <div>
+            <h3>{product.name}</h3>
+            <h3>{product.price}</h3>
+            <h3>{product.sku}</h3>
+            <img src={product.image} />
+            {product.attributes ? (
+              <div>
+                <p>{product.attributes?.description}</p>
+                <p>{product.attributes?.hardiness}</p>
+                <p>{product.attributes?.shape}</p>
+                <p>{product.attributes?.taste}</p>
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
+        ) : (
+          ""
+        )}
+      </section>
+    </section>
   );
 };
 
-export default ProductItem;
+export default ProductPage;
