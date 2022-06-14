814ec7aeda9db1d7e761aa867f499e8faf3085d5
Maria Bohorquez <maria.bohorquez@ceiba.com.co>
2022-06-14 12:01:44 -0500

    feature: add Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
 
 export const Home = () => {
   return (
     <div>
-        <h1>Hola mundo platzi</h1>
+      <Navbar />
+      <h1>Hola mundo platzi</h1>
     </div>
   );
 };
--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,10 +1,12 @@
 import { useRouter } from "next/router";
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar"
 
 const ProductItem = () => {
   const router = useRouter();
   return (
     <div>
+      <Navbar />
       <h1>Pagina para el producto {router.query.id}</h1>
     </div>
   );
